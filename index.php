42378712b2dfe772e4f129fc81650075b51b8385
vbushuev <v.bushuev@gmail.com>
2017-06-14 17:49:27 +0300

    release -1.0

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ ob_start();
 use core\Log as Log;
 use carbazar\parse\VIN as VIN;
 use carbazar\Auth as Auth;
+use carbazar\Request as Request;
 $resp = [
     "code"=>200,
     "status"=>"success",
@@ -26,9 +27,9 @@ while($resp["code"]==200){
     if(!isset($_REQUEST["action"]) || !in_array($_REQUEST["action"],["request","auth","status"]) ){$resp = setErrorResponse(500,"Unknown request type.");break;}
     $action = $_REQUEST["action"];
     if($action == "auth"){
-        if(!isset($_REQUEST["apikey"])){$resp = setErrorResponse(500,"No required field 'apikey'");break;}
-        if(!isset($_REQUEST["login"])){$resp = setErrorResponse(500,"No required field 'login'");break;}
-        if(!isset($_REQUEST["password"])){$resp = setErrorResponse(500,"No required field 'password'");break;}
+        if(!isset($_REQUEST["apikey"])){$resp = setErrorResponse(500,"No required field apikey");break;}
+        if(!isset($_REQUEST["login"])){$resp = setErrorResponse(500,"No required field login");break;}
+        if(!isset($_REQUEST["password"])){$resp = setErrorResponse(500,"No required field password");break;}
         $auth = new Auth([
             "apikey"=>$_REQUEST["apikey"],
             "login"=>$_REQUEST["login"],
@@ -38,15 +39,19 @@ while($resp["code"]==200){
         if($resp["code"]!=200)break;
     }
     elseif($action == "request"){
-        if(!isset($_REQUEST["session"])){$resp = setErrorResponse(401,"No required field 'session'");break;}
+        if(!isset($_REQUEST["session"])){$resp = setErrorResponse(401,"No required field session");break;}
         $auth = new Auth([
             "session"=>$_REQUEST["session"]
         ]);
         $resp = $auth->getResp();
         if($resp["code"]!=200)break;
-        if(!isset($_REQUEST["vin"])){$resp = setErrorResponse(500,"No required field 'vin'");break;}
+        $request = new Request(["session_id"=>$auth->getSession()->id,"vin"=>"","code"=>$resp["code"],"status"=>"progress","data"=>""]);
+        if(!isset($_REQUEST["vin"])){$resp = setErrorResponse(500,"No required field vin");$request->update(["code"=>$resp["code"],"status"=>$resp["status"],"message"=>$resp["message"]]);break;}
         $vin = strtoupper($_REQUEST["vin"]);
-        if(!VIN::validate($vin)){$resp = setErrorResponse(500,"Incorrect field value 'vin'");break;}
+        $request->update(["vin"=>$vin]);
+        $rq = ["vin"=>$vin];
+        if(!VIN::validate($vin)){$resp = setErrorResponse(500,"Incorrect field value vin");$request->update(["code"=>$resp["code"],"status"=>$resp["status"],"message"=>$resp["message"]]);break;}
+
         $result = [];
         $rca = new carbazar\parse\Rca();
         $zalog = new carbazar\parse\Zalog();
@@ -78,7 +83,9 @@ while($resp["code"]==200){
             //print_r($cpdata);exit;
             $result["carprice"]=json_decode($cp->get($cpdata),true);
         }
+        $request->update(["code"=>$resp["code"],"status"=>$resp["status"],"data"=>json_encode($resp,JSON_UNESCAPED_UNICODE)]);
         $resp["response"] = $result;
+        if($result["history"]["status"]=="200")$auth->getApikey()->decrease();
     }
     elseif($action == "status"){}
 
